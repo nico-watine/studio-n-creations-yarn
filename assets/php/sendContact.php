d82b0f99288495f69d7322a091398b57cd66cf58
Nico Watine <nico@studioncreations.com>
2021-03-20 12:48:35 -0500

    Copying files between repos

---
+++ assets/php/sendContact.php
@@ -0,0 +1,12 @@
+<?php 
+// $ToEmail = "email@ddress.com"; 
+// $EmailSubject = "Website Message!" . "\n"; 
+// $mailheader = "From: " . $_POST["email"] . "\n";
+// $mailheader .= "Reply-To: " . $_POST["email"] . "\n";  
+// $MESSAGE_BODY = "Name: " . "\n" . $_POST["name"] . "\n"; 
+// $MESSAGE_BODY .= "Email: " . "\n" . $_POST["email"] . "\n";
+// $MESSAGE_BODY .= "Message: " . "\n" . $_POST["message"] . "\n"; 
+// mail($ToEmail, $EmailSubject, $MESSAGE_BODY, $mailheader) or die ("Error!"); 
+
+// header("Location: /#thank-you");
+?>
